fdfb7f364df28a6833cb36a9e3fb8617595b050f
Yzee <yan.zhigurs@mail.ru>
2023-03-10 17:39:32 +0400

    layout and hierarchy

--- AlamofireHW/TableViewController.swift
+++ AlamofireHW/TableViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import Alamofire
 
 class TableViewController: UIViewController {
 
+    private var newCard = [Card]()
+    
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.dataSource = self
@@ -20,11 +23,34 @@ class TableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func fetchForCards() {
+        let request = AF.request("https://api.magicthegathering.io/v1/cards")
+        request.responseDecodable(of: Cards.self) { (data) in
+            guard let car = data.value else { return }
+            let cards = car.cards
+            self.newCard = cards
+            self.tableView.reloadData()
+        }
+    }
+    
+    func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+    }
 }
 
 extension TableViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return newCard.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
