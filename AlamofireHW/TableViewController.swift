27262f09b58dda31758a1b4b45859e22915dea98
Yzee <yan.zhigurs@mail.ru>
2023-03-10 17:30:19 +0400

    UITableView

--- AlamofireHW/TableViewController.swift
+++ AlamofireHW/TableViewController.swift
@@ -9,7 +9,29 @@ import UIKit
 
 class TableViewController: UIViewController {
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension TableViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
