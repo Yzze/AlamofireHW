eaa3c7dcb279f1d1ea29eca4a46ff83bc1e0b5e5
Yzee <yan.zhigurs@mail.ru>
2023-03-10 17:53:10 +0400

    add ui elements

--- AlamofireHW/DetailView.swift
+++ AlamofireHW/DetailView.swift
@@ -9,4 +9,36 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    private lazy var name: UILabel = {
+        var name = UILabel()
+        name.font = .boldSystemFont(ofSize: 20)
+        name.translatesAutoresizingMaskIntoConstraints = false
+        return name
+    }()
+    
+    private lazy var artist: UILabel = {
+        var artist = UILabel()
+        artist.font = .systemFont(ofSize: 15)
+        artist.translatesAutoresizingMaskIntoConstraints = false
+        return artist
+    }()
+    
+    private lazy var setName: UILabel = {
+        var setName = UILabel()
+        setName.font = .systemFont(ofSize: 15)
+        setName.textAlignment = .center
+        setName.contentMode = .scaleToFill
+        setName.numberOfLines = 0
+        setName.translatesAutoresizingMaskIntoConstraints = false
+        return setName
+    }()
+    
+    private lazy var text: UILabel = {
+        let text = UILabel()
+        text.textAlignment = .center
+        text.numberOfLines  = 5
+        text.font = .italicSystemFont(ofSize: 16)
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
 }
